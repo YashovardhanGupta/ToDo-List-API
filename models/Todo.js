9907ce4abe7f99141fe6c744c7f6e893da51abab
YashovardhanGupta <140542598+YashovardhanGupta@users.noreply.github.com>
2026-02-02 16:58:33 +0530

    Server Set Up and Model Creation (Schemas)

---
+++ models/Todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+const { title } = require('process');
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: true,
+    },
+    description: {
+        type: String, 
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
